94ca8e490109d503ee722ca47c5a293439667e0f
RBG DEV <149433862+rbgdevx@users.noreply.github.com>
2024-01-09 01:36:05 -0500

    adding slash command support

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # RaidFadeEvenMore
 
+## [v1.0.1](https://github.com/rbgdevx/raid-fade-even-more/releases/tag/v1.0.1) (2024-01-09)
+
+- adding slash command support
+
 ## [v1.0.0](https://github.com/rbgdevx/raid-fade-even-more/releases/tag/v1.0.0) (2024-01-08)
 
 - initial release
--- RaidFadeEvenMore.lua
+++ RaidFadeEvenMore.lua
@@ -4,15 +4,21 @@ local Interface = NS.Interface
 
 local LibStub = LibStub
 
-RaidFadeEvenMore = LibStub("AceAddon-3.0"):NewAddon(AddonName, "AceEvent-3.0")
+RaidFadeEvenMore = LibStub("AceAddon-3.0"):NewAddon(AddonName, "AceEvent-3.0", "AceConsole-3.0")
 
 function RaidFadeEvenMore:PLAYER_ENTERING_WORLD()
   Interface:Initialize()
 end
 
+function RaidFadeEvenMore:SlashCommands(_)
+  LibStub("AceConfigDialog-3.0"):Open(AddonName)
+end
+
 function RaidFadeEvenMore:OnInitialize()
   self.db = LibStub("AceDB-3.0"):New(AddonName .. "DB", NS.DefaultDatabase, true)
   self:SetupOptions()
+  self:RegisterChatCommand(AddonName, "SlashCommands")
+  self:RegisterChatCommand("rfem", "SlashCommands")
 end
 
 function RaidFadeEvenMore:OnEnable()
